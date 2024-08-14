14098c3f7c1e9171fa02a3e8496c315e1aed3e45
riyad022tagiyevv <97640853+riyad022tagiyevv@users.noreply.github.com>
2024-08-14 13:26:50 +0400

    Update env.py

--- config/env.py
+++ config/env.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = getenv('BOT_TOKEN')
-MONGO_URI = getenv('MONGO_URI')
-SUDO_USERS = list(map(int, getenv('SUDO_USERS').split()))
+BOT_TOKEN = getenv('BOT_TOKEN', "7237448561:AAG0upxhbub0Rfqrpx6gDfEOc9zU-tpUknQ")
+MONGO_URI = getenv('MONGO_URI', "mongodb+srv://agautevdragitevsvh:pJSptT6jE0pcw9a4@cluster0.de4uc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+SUDO_USERS = list(map(int, getenv('SUDO_USERS', "6634423600").split()))
